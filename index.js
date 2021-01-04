e11957901a436836c24f998a82edb5326fcd6d5b
Eunju/kiori <eun01300714@gmail.com>
2021-01-04 11:04:49 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.locals.pretty = true;
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', (req, res)=> {
+    res.render('main');
+});
+
+http.listen(3000, ()=> {
+    console.log('Connect at 3000');
+})
