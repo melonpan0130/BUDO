74008ecca0480f446f04d771f5659abac1ec8cf1
Eunju/kiori <eun01300714@gmail.com>
2021-01-04 18:09:28 +0900

    modified .gitignore file

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
-var app = require('express')();
-var http = require('http').Server(app);
-
-app.locals.pretty = true;
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const fs = require('fs');
+const spawn = require('child_process').spawn;
+const bodyParser = require('body-parser');
 app.set('view engine', 'pug');
 app.set('views', './views');
-
-app.get('/', (req, res)=> {
-    res.render('test');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+ 
+app.get('/',function(req,res){
+    res.render("test");
+});
+ 
+app.post('/form_receive',function(req,res) {
+    var code = req.body.code;
+    var source = code.split(/\r\n|\r\n/).join("\n");
+    var file='test.c';
+    console.log('source : '+source);
+    
+    fs.writeFile(file,source,'utf8',function(error) {
+        console.log('write end');
+    });
+    var compile = spawn('gcc',[file]);
+    compile.stdout.on('data',function(data) {
+        console.log('stdout: '+data);
+    });
+    compile.stderr.on('data',function(data){
+        console.log(String(data));
+    });
+    compile.on('close',function(data){
+        if(data ==0) {
+            var run = spawn('./a.exe',[]);    
+            run.stdout.on('data',function(output){
+                console.log('Complete compile.');
+                var responseData = {'result':'ok','output': output.toString('utf8')};
+                res.json(responseData);
+            });
+            run.stderr.on('data', function (output) {
+                console.log(String(output));
+            });
+            run.on('close', function (output) {
+                console.log('stdout: ' + output);
+            });
+        }
+    });
 });
 
 http.listen(3000, ()=> {
